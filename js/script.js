99b1f0ffde2282837ea4c831bcda7aefdb646659
David Richard <elidovrich.coding@gmail.com>
2022-03-24 13:09:34 -0400

    portfolio 1.0

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let btn = document.getElementById('click');
+
+// function changeOnClick (){
+//     btn.style.backgroundColor = 'red';
+
+// } 
+
+// btn.onclick = changeOnClick;
+
+
+
+
+
+
+
+// // 👇️ Change text color on mouseover
+// btn.addEventListener('mouseover', function handleMouseOver() {
+//     btn.style.backgroundColor = '#00C598';
+//   });
+  
+//   // 👇️ Change text color back on mouseout
+//   btn.addEventListener('mouseout', function handleMouseOut() {
+//     btn.style.backgroundColor = '#0C6567';
+//   });
