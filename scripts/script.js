6c0c7b4189e1cbcc8b844af05c0ac7a3cc0e0934
David Richard <elidovrich.coding@gmail.com>
2022-08-24 16:34:54 -0400

    add mobile proj overlay script

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,90 @@
-let btn = document.getElementById('click');
+const projImgBudget = document.getElementById("budget-project-img");
+const budget = document.getElementById("budget-project-overlay");
+const btnContainerBudget = document.getElementById("budget-btn-container");
+const projDescBudget = document.getElementById("budget-project-description");
 
-// function changeOnClick (){
-//     btn.style.backgroundColor = 'red';
+const projImgJam = document.getElementById("jam-project-img");
+const jam = document.getElementById("jam-project-overlay");
+const btnContainerJam = document.getElementById("jam-btn-container");
+const projDescJam = document.getElementById("jam-project-description");
 
-// } 
+const projImgReddit = document.getElementById("reddit-project-img");
+const reddit = document.getElementById("reddit-project-overlay");
+const btnContainerReddit = document.getElementById("reddit-btn-container");
+const projDescReddit = document.getElementById("reddit-project-description");
 
-// btn.onclick = changeOnClick;
+let budgetIsHidden;
+let jamIsHidden;
+let redditIsHidden;
 
+if (window.innerWidth <= 900) {
+  btnContainerBudget.style.display = "none";
+  projDescBudget.style.display = "none";
+  budgetIsHidden = true;
 
+  btnContainerJam.style.display = "none";
+  projDescJam.style.display = "none";
+  jamIsHidden = true;
 
+  btnContainerReddit.style.display = "none";
+  projDescReddit.style.display = "none";
+  redditIsHidden = true;
 
+  console.log("mobile");
+} else {
+  console.log("desktop");
+}
 
+const hideDetails = (btnContainer, projDesc, projImg) => {
+  btnContainer.style.display = "none";
+  projDesc.style.display = "none";
+  projImg.classList.remove("fx");
+};
 
+const revealDetails = (btnContainer, projDesc, projImg) => {
+  btnContainer.style.display = "flex";
+  projDesc.style.display = "flex";
+  projImg.classList.add("fx");
+};
 
-// // 👇️ Change text color on mouseover
-// btn.addEventListener('mouseover', function handleMouseOver() {
-//     btn.style.backgroundColor = '#00C598';
-//   });
-  
-//   // 👇️ Change text color back on mouseout
-//   btn.addEventListener('mouseout', function handleMouseOut() {
-//     btn.style.backgroundColor = '#0C6567';
-//   });
+budget.addEventListener("click", () => {
+  if (budgetIsHidden) {
+    revealDetails(btnContainerBudget, projDescBudget, projImgBudget);
+    hideDetails(btnContainerJam, projDescJam, projImgJam);
+    hideDetails(btnContainerReddit, projDescReddit, projImgReddit);
+    budgetIsHidden = false;
+    jamIsHidden = true;
+    redditIsHidden = true;
+  } else {
+    hideDetails(btnContainerBudget, projDescBudget, projImgBudget);
+    budgetIsHidden = true;
+  }
+});
+
+jam.addEventListener("click", () => {
+  if (jamIsHidden) {
+    revealDetails(btnContainerJam, projDescJam, projImgJam);
+    hideDetails(btnContainerBudget, projDescBudget, projImgBudget);
+    hideDetails(btnContainerReddit, projDescReddit, projImgReddit);
+    jamIsHidden = false;
+    budgetIsHidden = true;
+    redditIsHidden = true;
+  } else {
+    hideDetails(btnContainerJam, projDescJam, projImgJam);
+    jamIsHidden = true;
+  }
+});
+
+reddit.addEventListener("click", () => {
+  if (redditIsHidden) {
+    revealDetails(btnContainerReddit, projDescReddit, projImgReddit);
+    hideDetails(btnContainerJam, projDescJam, projImgJam);
+    hideDetails(btnContainerBudget, projDescBudget, projImgBudget);
+    redditIsHidden = false;
+    budgetIsHidden = true;
+    jamIsHidden = true;
+  } else {
+    hideDetails(btnContainerReddit, projDescReddit, projImgReddit);
+    redditIsHidden = true;
+  }
+});
